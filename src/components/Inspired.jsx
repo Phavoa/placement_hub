dc83d8ea4ea5dbff36d0be8a75eba2b461dc2d31
Efemiaya Favour Tega <efemiayafavour@gmail.com>
2025-12-24 08:42:10 +0100

    added join now

--- src/components/Inspired.jsx
+++ src/components/Inspired.jsx
@@ -1,26 +1,41 @@
 import React from "react";
 import TransitionImg from "../assets/transition-img.png";
 
-
 function Inspired() {
   return (
     <section className="inspired">
       <div className="inspired-head">
-        <h1 className="inspire">Get inspired by our <br />community</h1>
-         <a href="https://chat.whatsapp.com/Iv2UqRBlXTI1olmIOxdyWx?fbclid=IwQ0xDSwL21F1jbGNrAvbUWWV4dG4DYWVtAjExAAEeghIt3nNTRlRkQpvWWRDn4VdTg7RVMCOmZSZ9QNIaI4DtNhWs4qiWI5rfOVY_aem_98MwvqWv6CMz5i2uwrWuMQ" target="_blank"><button>Join Now</button></a>
+        <h1 className="inspire">
+          Get inspired by our <br />
+          community
+        </h1>
+        <a
+          href="https://chat.whatsapp.com/GqOVZloiq2Z7qwVZCcxtJX"
+          target="_blank"
+        >
+          <button>Join Now</button>
+        </a>
       </div>
 
       {/* <div className="inspired-content">
         <h1>Videos</h1>
       </div> */}
 
-
       <div className="transition">
         <div className="transition-content">
           <h1 className="transit">Transition into Tech Today!</h1>
-          <h2>Register now and take the first step towards a <br /> brighter future!</h2>
-          <p>Join us at Placement-Hub Academy and embark on a journey to <br />unlock your full potential in the dynamic world of IT and <br />entrepreneurship.</p>
-          <a href="/contact"><button>Register Now!</button></a>
+          <h2>
+            Register now and take the first step towards a <br /> brighter
+            future!
+          </h2>
+          <p>
+            Join us at Placement-Hub Academy and embark on a journey to <br />
+            unlock your full potential in the dynamic world of IT and <br />
+            entrepreneurship.
+          </p>
+          <a href="/contact">
+            <button>Register Now!</button>
+          </a>
         </div>
 
         <div className="transition-image">
@@ -29,6 +44,6 @@ function Inspired() {
       </div>
     </section>
   );
-};
+}
 
 export default Inspired;
